699312ef3a1d746c057a03d2f2698a95edc12913
Valexr <valexr@gmail.com>
2023-02-02 18:14:41 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" context="module">
+    import type { Header } from "$types";
+</script>
+
+<script lang="ts">
+    let header: Header = "esbuild-svelte-ts";
+</script>
+
+<main>
+    <h1>{header}</h1>
+</main>
+
+<style>
+    h1 {
+        color: red;
+        text-align: center;
+    }
+</style>
