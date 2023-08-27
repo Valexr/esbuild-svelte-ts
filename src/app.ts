15746e34aac8b3c5e8f4b3165006a8f51485cfb5
Valexr <valexr@gmail.com>
2023-08-27 08:21:20 +0300

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
 import { name, repository } from 'package.json'
 import App from './App.svelte';
 
-var app = new App({
+export default new App({
     target: document.body,
     props: { name, repository }
 });
-
-export default app;
